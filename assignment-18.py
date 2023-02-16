47911e5d44c4418d287275dcc0edf2308697e548
Philip Mueller <mail@philip.in-aachen.net>
2023-02-16 18:18:39 -0500

    assignment 18 complete. hopefully.

--- assignment-18.py
+++ assignment-18.py
@@ -2,14 +2,17 @@ import random
 
 import networkx as nx
 import matplotlib.pyplot as plt
+import numpy as np
+import math
 
 # replicating the results of the paper
 # "Structure, Scaling, and Phase Transition in the Optimal Transport Network"
 # by Steffen Bohn and Marcelo O. Magnasco, published 21 Feb 2007 in Physical Review Letters.
 
 n = 8
-gamma = 1
+gamma = 2
 i0 = 1000
+steps = 50
 
 # triangular lattice graph in the shape of a hexagon, side length n (nodes)
 def hexagon(n):
@@ -51,14 +54,40 @@ def hexagon(n):
 
     return G
 
-def draw_graph(G):
+def edge_width(G, edge):
+    max_conductance = max([G.edges[edge]['conductance'] for edge in G.edges()])
+
+    # normalize
+    width = G.edges[edge]['conductance']/max_conductance
+
+    # cut off very small values
+    if width < 0.001:
+        width = 0
+
+    # sublinear scaling
+    width = math.sqrt(width)
+
+    # linear scaling
+    width *= 10
+
+    return width
+
+def draw_graph(G, nodes=True):
     # draw graph, with edge width proportional to conductance
     pos = nx.get_node_attributes(G, 'pos')
+    # make canvas a little taller than wide
+    plt.figure(figsize=(6, 7))
     nx.draw(G, pos, with_labels=False,
-        width=[G.edges[edge]['conductance']/total_conductance*100*4 for edge in G.edges()],
-        node_size=0,
+        width=[edge_width(G, edge) for edge in G.edges()],
+        # node size is absolute value of potential or 0 if nodes=False
+        node_size=[abs(G.nodes[node]['potential']) if nodes else 0 for node in G.nodes()],
+        # node color is sign of potential: green for positive, red for negative
+        node_color=['g' if G.nodes[node]['potential'] > 0 else 'r' for node in G.nodes()],
     )
     plt.show()
+
+def calculate_total_conductance(G):
+    return sum([G.edges[edge]['conductance']**gamma for edge in G.edges()])**(1/gamma)
 
 G = hexagon(n)
 
@@ -77,9 +106,48 @@ for edge in G.edges():
     G.edges[edge]['conductance'] = random.random()
 
 # total conductance of the graph
-total_conductance = sum([G.edges[edge]['conductance']**gamma for edge in G.edges()])**(1/gamma)
+total_conductance = calculate_total_conductance(G)
 
-# print adjacency matrix with node labels
-print(nx.to_pandas_adjacency(G, weight='conductance'))
+# type sig:
+def determine_potential(G: nx.Graph):
+    '''
+    determine the potential at each node based on the conductances and the current sources
+    Use sources = (conductances_diagonal - conductances_offdiagonal) * potentials
+    '''
+    sources = np.array([G.nodes[node]['source'] for node in G.nodes()])
+    conductances_offdiagonal = nx.adjacency_matrix(G, weight='conductance')
 
-draw_graph(G)
+    conductances_diagonal = np.identity(len(G.nodes()))
+    np.fill_diagonal(conductances_diagonal, conductances_offdiagonal.sum(axis=1))
+
+    # solve the linear system
+    potentials = np.linalg.solve(conductances_diagonal - conductances_offdiagonal, sources)
+
+    return potentials
+
+def step(G):
+    # determine the potential at each node
+    potentials = determine_potential(G)
+
+    # set the potentials as node attributes
+    for i, node in enumerate(G.nodes()):
+        G.nodes[node]['potential'] = potentials[i]
+
+    # current through each edge
+    for edge in G.edges():
+        G.edges[edge]['current'] = G.edges[edge]['conductance'] * (G.nodes[edge[0]]['potential'] - G.nodes[edge[1]]['potential'])
+
+    # compute a new set of conductances by following the scaling
+    for edge in G.edges():
+        G.edges[edge]['conductance'] = abs(G.edges[edge]['current'])**(-(Gamma-2))
+
+    # normalize conductances
+    new_total_conductance = calculate_total_conductance(G)
+    for edge in G.edges():
+        G.edges[edge]['conductance'] *= total_conductance/new_total_conductance
+
+for i in range(steps):
+    step(G)
+
+# draw_graph(G)
+draw_graph(G, nodes=False)
